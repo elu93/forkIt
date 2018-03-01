e701e9a5d90de59894b95e2c3ce64a6bb4faa66a
ericstotle93 <elu1993@gmail.com>
2018-02-28 21:05:17 -0500

    Added styling to restaurants part of app

--- client/src/components/Restaurant.js
+++ client/src/components/Restaurant.js
@@ -3,19 +3,27 @@ import {Link} from 'react-router-dom'
 import styled from 'styled-components'
 import {CardPanel} from "react-materialize";
 
+const InlineHeader = styled.h2`
+    display: inline;
+    padding: 5%;
+`
+
+const ParagraphSize = styled.p`
+    font-size: .3em;
+`
+
 const Restaurant = (props) => {
 
     return (
         <div>
-            <CardPanel>
-                <span><img
+            <CardPanel className="valign-wrapper">
+                <img
                     width="200"src={`https://maps.googleapis.com/maps/api/staticmap?center=${props.latitude},${props.longitude}&zoom=12&size=400x400&markers=color:red|${props.latitude},${props.longitude}&key=AIzaSyCRiXIAL7OPuPSVUB7O1q1SlZ_QFONdavo `}/>
-                <h2>
+                <InlineHeader>
                     <Link to={`/restaurants/${props.id}`}>{props.name}</Link>
-                </h2></span>
+                    <ParagraphSize>Rating: {props.rating}</ParagraphSize>
+                </InlineHeader>
             </CardPanel>
-            <div></div>
-            <div>{props.rating}</div>
         </div>
     )
 }
--- client/src/components/RestaurantShow.js
+++ client/src/components/RestaurantShow.js
@@ -1,6 +1,18 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import axios from 'axios'
 import {Link} from 'react-router-dom'
+import styled from 'styled-components'
+import {Row, CardPanel, Col} from "react-materialize";
+
+const CardDiv = styled.div `     
+    margin: 0 auto;     
+    width: 60vw;    
+    display: flex;     
+    justify-content: center;
+    align-items: center;
+    flex-direction: column; 
+    text-align: center;
+`
 
 class RestaurantShow extends Component {
     state = {
@@ -21,15 +33,17 @@ class RestaurantShow extends Component {
         const postArray = []
         const id = parseInt(this.props.match.params.id)
 
-        resPost.data.map((post) => {
-            if(post.restaurant_id === id) {
-                postArray.push(post)
-            }
-        })
+        resPost
+            .data
+            .map((post) => {
+                if (post.restaurant_id === id) {
+                    postArray.push(post)
+                }
+            })
         postArray.reverse()
         this.setState({restaurant: response.data, posts: postArray, users: resUsers.data})
         console.log(this.state.users)
-        
+
     }
 
     addNewPost = (newPost) => {
@@ -42,20 +56,24 @@ class RestaurantShow extends Component {
     handleChange = (event) => {
         const attribute = event.target.name
         const val = event.target.value
-        const newPost = {...this.state.newPost}
+        const newPost = {
+            ...this.state.newPost
+        }
         newPost[attribute] = val
         this.setState({newPost})
     }
 
     newPostPost = () => {
-        axios.post(`/posts`, this.state.newPost)
-        .then((response) => {
-            const updateNewPost = this.state.newPost
-            updateNewPost._id = response.data._id
-            this.addNewPost(updateNewPost)
-        }).catch((error) => {
-            console.log(error)
-        })
+        axios
+            .post(`/posts`, this.state.newPost)
+            .then((response) => {
+                const updateNewPost = this.state.newPost
+                updateNewPost._id = response.data._id
+                this.addNewPost(updateNewPost)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
     }
 
     handleSubmit = (event) => {
@@ -65,27 +83,43 @@ class RestaurantShow extends Component {
 
     render() {
         return (
-            <div>
+            <CardDiv>
                 <h1>{this.state.restaurant.name}</h1>
-                <img src={this.state.restaurant.image} />
-                <div><Link to={`/restaurants/${this.state.restaurant.id}/foods`}>View Menu</Link></div>
+                <img width="400" src={this.state.restaurant.image}/>
+                <div>
+                    <Link to={`/restaurants/${this.state.restaurant.id}/foods`}>View Menu</Link>
+                </div>
                 <Link to="/posts/new">Add a Post</Link>
                 <form onSubmit={this.handleSubmit}>
-                <input name="title" placeholder="Title" type="text" onChange={this.handleChange}/>
-                <input name="content" placeholder="Content" type="text" onChange={this.handleChange}/>
-                <button type="submit" value="submit">Submit</button>
+                    <input
+                        name="title"
+                        placeholder="Title"
+                        type="text"
+                        onChange={this.handleChange}/>
+                    <input
+                        name="content"
+                        placeholder="Content"
+                        type="text"
+                        onChange={this.handleChange}/>
+                    <button type="submit" value="submit">Submit</button>
                 </form>
-                {
-                    this.state.posts.map((post) => {
+                {this
+                    .state
+                    .posts
+                    .map((post) => {
                         return (
-                            <div>
-                                <h3>{post.title}</h3>
-                                <p>{post.content}</p>
-                            </div>
+                            <Row>
+                                <Col s={12} m={12}>
+                                    <CardPanel m={12} className="left-align">
+                                        <h3>{post.title}</h3>
+                                        <p>{post.content}</p>
+                                    </CardPanel>
+                                </Col>
+                            </Row>
                         )
                     })
-                }
-            </div>
+}
+            </CardDiv>
         );
     }
 }
